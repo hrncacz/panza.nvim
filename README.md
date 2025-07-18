3253f17d17549df07939e2402bd7d5c9cf443f2a
hrncacz <hrncacz@gmail.com>
2025-07-18 18:54:47 +0200

    added initializing venv and pip check

---
+++ README.md
@@ -0,0 +1,8 @@
+# PANZA.NVIM
+
+## Description
+
+- Local AI agent inside Neovim
+- Runnig on xxx LLM
+- Helpful assistant for starting programmers
+- No file editing
---
+++ lua/panza.lua
@@ -0,0 +1,64 @@
+local M = {}
+
+
+local config = {
+	root_path = "", -- root forlder of plugin folder
+	venv = "",     -- python venv folder path
+	python = "",   -- python3 venv path
+	pip = "",      -- pip venv path
+}
+
+M.setup = function() -- todo
+end
+
+local install_requirements = function()
+	-- local output = io.popen(config.pip .. " install -r requirements.txt")
+	local output = io.popen(config.python .. " -m pip check"):read("*a")
+	print(output)
+	print("hhh")
+end
+
+local create_python_venv = function()
+	local output = os.execute("python3 -m venv " .. config.venv)
+	if not output == 0 then
+		return
+	end
+	config.python = vim.fs.joinpath(config.venv, "bin", "python3")
+	config.pip = vim.fs.joinpath(config.venv, "bin", "pip")
+	install_requirements()
+end
+
+
+local test_dependencies = function()
+	-- Finding root_path of this plugin
+	local mod_path = vim.loader.find("panza")[1].modpath
+	for dir in vim.fs.parents(mod_path) do
+		if vim.fn.filereadable(vim.fs.joinpath(dir, "lua", "panza.lua")) == 1 then
+			config.root_path = dir
+			break
+		end
+	end
+	-- Checking existence of python venv
+	config.venv = vim.fs.joinpath(config.root_path, "agent", "venv")
+	if vim.fn.isdirectory(config.venv) == 1 then
+		config.python3 = vim.fs.joinpath(config.venv, "bin", "python3")
+		config.pip = vim.fs.joinpath(config.venv, "bin", "pip")
+	else
+		create_python_venv()
+	end
+end
+
+M.load_module = function()
+	local test_string = { text = "Sancho is ready" }
+	local status, result = pcall(vim.json.encode, test_string)
+	test_dependencies()
+	if not status then
+		print("Error decoding json: " .. result)
+	else
+		print(result)
+	end
+end
+
+M.load_module()
+
+return M
