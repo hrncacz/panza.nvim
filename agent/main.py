e4c2256c365c8cebc9095d5e9d2b8aa2e08d6186
hrncacz <hrncacz@gmail.com>
2025-08-01 14:35:54 +0200

    dynamic device type - CPU vs GPU in AI agent

--- agent/main.py
+++ agent/main.py
@@ -4,8 +4,6 @@ import sys
 from huggingface_hub import login
 from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline, BitsAndBytesConfig
 import torch
-#microsoft/Phi-3-mini-4k-instruct
-#teknium/OpenHermes-2.5-Mistral-7B
 def main():
     hf_api_key = None
     for i in sys.argv:
@@ -23,8 +21,7 @@ def main():
         bnb_4bit_use_double_quant=True,
         bnb_4bit_quant_type="nf4",
     )
-    # device = 0 if torch.cuda.is_available() else -1
-    device = -1
+    device = 0 if torch.cuda.is_available() else -1
     model_name = "teknium/OpenHermes-2.5-Mistral-7B"  # Change to your local path or HF model
     tokenizer = AutoTokenizer.from_pretrained(model_name, use_fast=False)
     model = AutoModelForCausalLM.from_pretrained(model_name, device_map="auto", use_safetensors=True, quantization_config=bnb_config)
